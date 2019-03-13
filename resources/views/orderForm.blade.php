cf198e6fe61d636a100e3ae813c6fb2f41d05cbf
Justin Mata <justinmata2@yahoo.com>
2019-03-13 00:35:37 -0700

    Created a basic front-end for placing orders
    You can see it by going to /order when you serve the project. Still needs a lot of work such as controllers so I can store the information into the database. Also not sure if there is any other information required for a restaurant to enter besides the current 3

---
+++ resources/views/orderForm.blade.php
@@ -0,0 +1,47 @@
+@extends('layout')
+
+@section('content')
+<h1 class='title'>Place an Order</h1>
+<form>
+    @csrf
+    <div class='field'>
+        <label class='label' for='delivery_name'>Delivery Name</label>
+
+        <div class='control'>
+            <input class='input' type='text' name='delivery_name' placeholder='Name for Delivery' value='{{ old('delivery_name') }}' required>
+        </div>
+    </div>
+
+    <div class='field'>
+        <label class='label' for='address_id'>Address ID</label>
+
+        <div class='control'>
+            <input class='input' type='text' name='address_id' placeholder='Address ID' value='{{ old('address_id') }}' required>
+        </div>
+    </div>
+
+    <div class='field'>
+        <label class='label' for='delivery_comment'>Comment</label>
+
+        <div class='control'>
+            <textarea class='textarea' name='delivery_comment' placeholder='Comment for Delivery' required>{{ old('delivery_comment') }}</textarea>
+        </div>
+    </div>
+
+    <div class='field'>
+        <div class='control'>
+            <button type='submit' class='button is-link'>Place Order</button>
+        </div>
+    </div>
+
+    @if($errors->any())
+    <div class='notification is-danger'>
+        <ul>
+            @foreach($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+</form>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/order', function () {
+    return view('orderForm');
+});
