7ce4b24a42be5f2882359b99bda15b1fdfdacc62
Justin Mata <justinmata2@yahoo.com>
2019-04-01 20:59:06 -0700

    Updated orderForm look
    Just a quick update to the orderForm so that it looks identical to our registration and login forms

--- resources/views/pages/orderForm.blade.php
+++ resources/views/pages/orderForm.blade.php
@@ -1,81 +1,88 @@
-@extends('layouts.default')
-
+@extends('layouts.default') 
 @section('content')
-<h1 class='title'>Place an Order</h1>
-<form method="POST" action="/cart">
-    @csrf
-    <div class='field'>
-        <label class='label' for='delivery_name'>Delivery Name</label>
+<div class="container">
+    <div class="row justify-content-center my-5">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Place an Order</div>
 
-        <div class='control'>
-            <input class='input' type='text' name='delivery_name' placeholder='Name for Delivery' value='{{ old('delivery_name') }}' required>
+                <div class="card-body">
+                    <form method="POST" action="/cart">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="delivery_name" class="col-md-4 col-form-label text-md-right">Delivery Name</label>
+                            <div class="col-md-6">
+                                <input id="delivery_name" type="text" class="form-control" name="delivery_name" value="{{ old('delivery_name') }}" required
+                                    autofocus>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="street1" class="col-md-4 col-form-label text-md-right">Street 1</label>
+                            <div class="col-md-6">
+                                <input id="street1" type="text" class="form-control" name="street1" value="{{ old('street1') }}" required autofocus>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="street2" class="col-md-4 col-form-label text-md-right">Street 2</label>
+                            <div class="col-md-6">
+                                <input id="street2" type="text" class="form-control" name="street2" value="{{ old('street2') }}" required autofocus>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="city" class="col-md-4 col-form-label text-md-right">City</label>
+                            <div class="col-md-6">
+                                <input id="city" type="text" class="form-control" name="city" value="{{ old('city') }}" required autofocus>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="state" class="col-md-4 col-form-label text-md-right">State</label>
+
+                            <div class="col-md-6">
+                                <select id="state" type="type" class="form-control" name="state" value="{{ old('state') }}" required>
+    @include('layouts.partials.states')
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="zip" class="col-md-4 col-form-label text-md-right">Zip Code</label>
+                            <div class="col-md-6">
+                                <input id="zip" type="text" class="form-control" name="zip" value="{{ old('zip') }}" required autofocus>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="delivery_comments" class="col-md-4 col-form-label text-md-right">Comment</label>
+                            <div class="col-md-6">
+                                <textarea id="delivery_comments" type="text" class="form-control" name="delivery_comments" rows="3" value="{{ old('delivery_comments') }}"
+                                    required autofocus></textarea>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">Place Order</button>
+                            </div>
+                        </div>
+
+                        @if($errors->any())
+                        <div class='notification is-danger'>
+                            <ul>
+                                @foreach($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                        @endif
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
-
-    <div class='field'>
-        <label class='label' for='street1'>Street 1</label>
-
-        <div class='control'>
-            <input class='input' type='text' name='street1' placeholder='Street 1' value='{{ old('street1') }}' required>
-        </div>
-    </div>
-
-    <div class='field'>
-        <label class='label' for='street2'>Street 2</label>
-
-        <div class='control'>
-            <input class='input' type='text' name='street2' placeholder='Street 2' value='{{ old('street2') }}'>
-        </div>
-    </div>
-
-    <div class='field'>
-        <label class='label' for='city'>City</label>
-
-        <div class='control'>
-            <input class='input' type='text' name='city' placeholder='City' value='{{ old('city') }}' required>
-        </div>
-    </div>
-
-    <div class="form-group row">
-        <label for="state" class="col-md-4 col-form-label text-md-right">State</label>
-
-        <div class="col-md-6">
-            <select id="state" type="type" class="form-control" name="state" value="{{ old('state') }}" required>
-                @include('layouts.partials.states')
-            </select>
-        </div>
-    </div>
-
-    <div class='field'>
-        <label class='label' for='zip'>Zip Code</label>
-
-        <div class='control'>
-            <input class='input' type='text' name='zip' placeholder='Zip Code' value='{{ old('zip') }}' required>
-        </div>
-    </div>
-
-    <div class='field'>
-        <label class='label' for='delivery_comments'>Comment</label>
-
-        <div class='control'>
-            <textarea class='textarea' name='delivery_comments' placeholder='Comment for Delivery'>{{ old('delivery_comments') }}</textarea>
-        </div>
-    </div>
-
-    <div class='field'>
-        <div class='control'>
-            <button type='submit' class='button is-link'>Place Order</button>
-        </div>
-    </div>
-
-    @if($errors->any())
-    <div class='notification is-danger'>
-        <ul>
-            @foreach($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-    @endif
-</form>
+</div>
 @endsection
